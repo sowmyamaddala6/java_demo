ac413f9362364b0d25bb617f5a221dfd0f11e9da
sowmyamaddala6 <sowmyamaddala6@gmail.com>
2024-12-14 12:23:01 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,126 @@
+/*class Hello{
+   // public static void main(String args[]){
+        //System.out.println("Haii");
+   // }
+}*/
+//import java.util.Scanner;
+
+//public class Hello {
+    //public static class Calculator {
+        //public void add(int a, int b) {
+            //System.out.println("Addition: " + (a + b));
+        //}
+        //public void sub(int a, int b) {
+            //System.out.println("Subtraction: " + (a - b));
+        //}
+        //public void mul(int a, int b) {
+            //System.out.println("Multiplication: " + (a * b));
+        //}
+    //}
+
+    //public static void main(String[] args) {
+        //Calculator calculator = new Calculator(); 
+        //Scanner sc = new Scanner(System.in);
+
+        //System.out.println("Enter a value:");
+        //int a = sc.nextInt(); 
+
+        //System.out.println("Enter b value:");
+        //int b = sc.nextInt(); 
+
+       // calculator.add(a, b);
+       // calculator.sub(a, b);
+       // calculator.mul(a, b);
+
+        //System.out.println("Thank you!");
+        //sc.close(); 
+   // }
+//}
+//JAVA PRACTICE
+//class Hello{
+    // public static void main(String[] args) {
+        //System.out.println("Hi ");
+
+    //}
+//}
+//Variables
+//class Hello{
+    //public static void main(String[] args) {
+       // System.out.println(6-5);
+       // System.out.println(8-5);
+
+   // }
+//}
+//class Hello{
+    //public static void main(String[] args) {
+        //int num1 = 6;
+       // int num2 = 3;
+       // int result = num1+num2;
+       // System.out.println(result);
+    //}
+//}
+//Data Types
+//class Hello{
+   // public static void main(String args[]){
+        //int num =6;
+        //byte by =3;
+        //short s =23;
+       // long l = 3456;
+       // float f = 5.8f;
+       // double d = 5.8;
+        //char c = 's';
+       // boolean b = true;
+
+    //}
+
+//}
+//literals
+//class Hello{
+    //public static void main(String[] args) {
+        //char c = 'a';
+       // c++;
+       // System.out.println(c);
+    //}
+//}
+//Type conversions and casting
+//class Hello{
+   // public static void main(String args[]){
+       // byte b =127;
+       // int a=255;
+       // byte k = (byte)a;
+       // float f = 5.6f;
+       // int t = (int)f;
+       //int a=6;
+       //byte k = (byte)a;
+       // System.out.println(a);
+   // }
+//}
+//Arithmetic operators in java
+//class Hello{
+   // public static void main(String[] args) {
+      //  int num1 = 6;
+       // num1 = num1+3;
+       // int num2 = 5;
+       // int result = num1%num2;
+       //num1 -=2;
+      // num1 ++;
+       // System.out.println(num1);
+    //}
+//}
+//Relational operators in java
+//class Hello{
+    //public static void main(String[] args) {
+        //int x=6;
+        //int y=5;
+        //boolean result = x!=y;
+        //System.out.println(result);
+    //}
+//}
+//class Hello{
+    //public static void main(String[] args) {
+       // double x = 5.6;
+        //double y = 6.5;
+        //boolean result = x>=y;
+       // System.out.println(result);
+    //}
+//}
